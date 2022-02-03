bb51d7ad0e3af3b9fe18c094129178f9b2f2171e
Mike <49212643+mbuslenko@users.noreply.github.com>
2022-02-04 00:59:27 +0200

    hexagon repair fix

--- src/domains/pixel/services/game.service.ts
+++ src/domains/pixel/services/game.service.ts
@@ -54,25 +54,25 @@ export class GameService {
 	// TODO: Refactor this function to reduce its Cognitive Complexity from 86 to the 15 allowed.
 	async attackHexagon(userId: string, props: AttackHexagonDto): Promise<void> {
 		const start = performance.now();
-		console.log(props)
+		console.log(props);
 
 		const attackerRow = await this.attackPixelRepository.findOne({
 			where: { numericId: props.from },
 		});
 
-		console.log(11111111111111111111111111111111111)
+		console.log(11111111111111111111111111111111111);
 
 		const attackerPixel = await this.pixelRepository.findOne({
 			where: { numericId: props.from },
 		});
 
-		console.log(2222222222222222222222222222222222222222222222222222)
+		console.log(2222222222222222222222222222222222222222222222222222);
 
 		const attackedPixel = await this.pixelRepository.findOne({
 			where: { numericId: props.to },
 		});
 
-		console.log(333333333333333333333333333333333333333333333333)
+		console.log(333333333333333333333333333333333333333333333333);
 
 		const attack = await this.attacksRepository.save(
 			this.attacksRepository.create({
@@ -82,7 +82,7 @@ export class GameService {
 			}),
 		);
 
-		console.log(444444444444444444444444444444444444444444444444444444)
+		console.log(444444444444444444444444444444444444444444444444444444);
 
 		this.eventsGateway.sendMessageForMap({
 			from: attackerPixel.numericId,
@@ -457,6 +457,12 @@ export class GameService {
 
 		// add logic here
 		const coinsToRepair = 0.5;
+
+		if (hexagon.type === 'defender') {
+			await this.defenderPixelRepository.update({ numericId }, { health: 100 });
+		} else if (hexagon.type === 'attack') {
+			await this.attackPixelRepository.update({ numericId }, { health: 100 });
+		}
 
 		await this.coinDomain.sendCoinsToPixold(userId, coinsToRepair);
 	}
