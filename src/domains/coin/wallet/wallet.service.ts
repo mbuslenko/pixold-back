e112a8d63788c1f9f760f97754f0c95add9f4815
Mike <49212643+mbuslenko@users.noreply.github.com>
2022-02-03 19:45:38 +0200

    new connect wallet response type

--- src/domains/coin/wallet/wallet.service.ts
+++ src/domains/coin/wallet/wallet.service.ts
@@ -1,7 +1,7 @@
 import {
-  HttpException,
-  Injectable,
-  InternalServerErrorException,
+	HttpException,
+	Injectable,
+	InternalServerErrorException,
 } from '@nestjs/common';
 
 import * as stellar from '../../../common/stellar';
@@ -11,216 +11,220 @@ import { WalletRepository } from './persistance/wallet.repository';
 import { ConnectWalletDto } from '../../../api/wallet/dto/wallet.dto';
 import { sendNotification } from '../../../common/utils/telegram-notifications';
 import {
-  sendTransactionToPixold,
-  sendTransactionToUser,
+	sendTransactionToPixold,
+	sendTransactionToUser,
 } from '../../../common/stellar/transaction';
 
 @Injectable()
 export class WalletService {
-  constructor(private readonly walletRepository: WalletRepository) {}
+	constructor(private readonly walletRepository: WalletRepository) {}
 
-  async connectWallet(props: ConnectWalletDto) {
-    const accountInfo = await stellar
-      .getAccountInfo(props.publicKey)
-      .catch((e) => {
-        if (e.status === 404) {
-          throw new HttpException(
-            { message: 'To connect a wallet, you must have a balance on it' },
-            400,
-          );
-        } else {
-          throw e;
-        }
-      });
+	async connectWallet(props: ConnectWalletDto) {
+		const accountInfo = await stellar
+			.getAccountInfo(props.publicKey)
+			.catch((e) => {
+				if (e.status === 404) {
+					throw new HttpException(
+						{ message: 'To connect a wallet, you must have a balance on it' },
+						400,
+					);
+				} else {
+					throw e;
+				}
+			});
 
-    for (const [key, value] of Object.entries(accountInfo.rawData.flags)) {
-      if (value === true) {
-        throw new HttpException(accountTypeIsNotSupported, 400);
-      }
-    }
+		for (const [key, value] of Object.entries(accountInfo.rawData.flags)) {
+			if (value === true) {
+				throw new HttpException(accountTypeIsNotSupported, 400);
+			}
+		}
 
-    const row = await this.walletRepository.findOne({
-      publicKey: props.publicKey,
-    });
+		const row = await this.walletRepository.findOne({
+			publicKey: props.publicKey,
+		});
 
-    if (!row) {
-      await this.walletRepository.delete({ ownerId: props.userId });
+		if (!row) {
+			await this.walletRepository.delete({ ownerId: props.userId });
 
-      const wallet = await this.walletRepository.save(
-        this.walletRepository.create({
-          publicKey: props.publicKey,
-          secretKey: props.secret,
-          ownerId: props.userId,
-          ...accountInfo,
-        }),
-      );
+			await this.walletRepository.save(
+				this.walletRepository.create({
+					publicKey: props.publicKey,
+					secretKey: props.secret,
+					ownerId: props.userId,
+					...accountInfo,
+				}),
+			);
 
-      return {
-        id: wallet.id,
-        balanceInUSD: wallet.balanceInUSD,
-        balanceInXLM: wallet.balanceInXLM,
-        balanceInPXL: wallet.balanceInPXL,
-      };
-    } else {
-      throw new HttpException({ message: 'Wallet already exists' }, 400);
-    }
-  }
+			const [wallet] = await this.walletRepository.getWallet(props.userId);
 
-  async getWallet(userId: string, throwError: boolean) {
-    const [wallet] = await this.walletRepository.getWallet(userId);
+			return {
+				username: wallet.username,
+				balanceInUSD: wallet.balance_in_usd,
+				balanceInXLM: wallet.balance_in_xlm,
+				balanceInPXL: wallet.balance_in_pxl,
+			};
+		} else {
+			throw new HttpException({ message: 'Wallet already exists' }, 400);
+		}
+	}
 
-    if (!wallet) {
-      if (throwError) {
-        throw new HttpException({ message: 'Wallet not found' }, 400);
-      }
+	async getWallet(userId: string, throwError: boolean) {
+		const [wallet] = await this.walletRepository.getWallet(userId);
 
-      return null;
-    }
+		if (!wallet) {
+			if (throwError) {
+				throw new HttpException({ message: 'Wallet not found' }, 400);
+			}
 
-    return {
-      username: wallet.username,
-      balanceInUSD: wallet.balance_in_usd,
-      balanceInXLM: wallet.balance_in_xlm,
-      balanceInPXL: wallet.balance_in_pxl,
-    };
-  }
+			return null;
+		}
 
-  async getPixoldCoinsLeft() {
-    return this.walletRepository
-      .findOne({ where: { ownerId: '6370046c-ac93-4d19-a55f-c59c5d5211b3' } })
-      .then((response) => {
-        return response.balanceInPXL;
-      });
-  }
+		return {
+			username: wallet.username,
+			balanceInUSD: wallet.balance_in_usd,
+			balanceInXLM: wallet.balance_in_xlm,
+			balanceInPXL: wallet.balance_in_pxl,
+		};
+	}
 
-  async sendCoinsToUser(userId: string, coins: number): Promise<void> {
-    const userWallet = await this.walletRepository.findOne({
-      where: { ownerId: userId },
-    });
-    const [pixoldWallet] = await this.walletRepository.getWallet('6370046c-ac93-4d19-a55f-c59c5d5211b3');
+	async getPixoldCoinsLeft() {
+		return this.walletRepository
+			.findOne({ where: { ownerId: '6370046c-ac93-4d19-a55f-c59c5d5211b3' } })
+			.then((response) => {
+				return response.balanceInPXL;
+			});
+	}
 
-    if (!userWallet) {
-      throw new HttpException(
-        { message: `You don't have wallet to send coins` },
-        400,
-      );
-    }
+	async sendCoinsToUser(userId: string, coins: number): Promise<void> {
+		const userWallet = await this.walletRepository.findOne({
+			where: { ownerId: userId },
+		});
+		const [pixoldWallet] = await this.walletRepository.getWallet(
+			'6370046c-ac93-4d19-a55f-c59c5d5211b3',
+		);
 
-    const newUserBalance = userWallet.balanceInPXL + coins;
-    const newPixoldBalance = pixoldWallet.balance_in_pxl - coins;
+		if (!userWallet) {
+			throw new HttpException(
+				{ message: `You don't have wallet to send coins` },
+				400,
+			);
+		}
 
-    if (newPixoldBalance < 10000) {
-      sendNotification(
-        `@mbuslenko, @myroslavvv, There are less than 10,000 PXL left on Pixold`,
-      );
-    } else if (newPixoldBalance < 1000) {
-      sendNotification(
-        `@mbuslenko, @myroslavvv, There are less than 1,000 PXL left on Pixold`,
-      );
-    } else if (newPixoldBalance < 0) {
-      throw new InternalServerErrorException({
-        message: `Not enough coins in Pixold`,
-      });
-    }
- 
-    // ! TODO: Uncomment
-    //sendTransactionToUser(userWallet.publicKey, coins.toString());
+		const newUserBalance = userWallet.balanceInPXL + coins;
+		const newPixoldBalance = pixoldWallet.balance_in_pxl - coins;
 
-    await this.walletRepository.update(
-      { id: userWallet.id },
-      { balanceInPXL: newUserBalance },
-    );
-    await this.walletRepository.update(
-      { ownerId: '6370046c-ac93-4d19-a55f-c59c5d5211b3' },
-      { balanceInPXL: newPixoldBalance },
-    );
-  }
+		if (newPixoldBalance < 10000) {
+			sendNotification(
+				`@mbuslenko, @myroslavvv, There are less than 10,000 PXL left on Pixold`,
+			);
+		} else if (newPixoldBalance < 1000) {
+			sendNotification(
+				`@mbuslenko, @myroslavvv, There are less than 1,000 PXL left on Pixold`,
+			);
+		} else if (newPixoldBalance < 0) {
+			throw new InternalServerErrorException({
+				message: `Not enough coins in Pixold`,
+			});
+		}
 
-  async substractCoinsFromPixoldBalance(coins: number): Promise<void> {
-    const pixoldWallet = await this.walletRepository.findOne({
-      where: { ownerId: 'pixold' },
-    });
+		// ! TODO: Uncomment
+		//sendTransactionToUser(userWallet.publicKey, coins.toString());
 
-    const newPixoldBalance = pixoldWallet.balanceInPXL - coins;
+		await this.walletRepository.update(
+			{ id: userWallet.id },
+			{ balanceInPXL: newUserBalance },
+		);
+		await this.walletRepository.update(
+			{ ownerId: '6370046c-ac93-4d19-a55f-c59c5d5211b3' },
+			{ balanceInPXL: newPixoldBalance },
+		);
+	}
 
-    if (newPixoldBalance < 10000) {
-      sendNotification(
-        `@mbuslenko, @myroslavvv, There are less than 10,000 PXL left on Pixold`,
-      );
-    } else if (newPixoldBalance < 1000) {
-      sendNotification(
-        `@mbuslenko, @myroslavvv, There are less than 1,000 PXL left on Pixold`,
-      );
-    } else if (newPixoldBalance < 0) {
-      throw new InternalServerErrorException({
-        message: `Not enough coins in Pixold`,
-      });
-    }
+	async substractCoinsFromPixoldBalance(coins: number): Promise<void> {
+		const pixoldWallet = await this.walletRepository.findOne({
+			where: { ownerId: 'pixold' },
+		});
 
-    await this.walletRepository.update(
-      { id: pixoldWallet.id },
-      { balanceInPXL: newPixoldBalance },
-    );
-  }
+		const newPixoldBalance = pixoldWallet.balanceInPXL - coins;
 
-  async substractCoinsFromUserBalance(
-    userId: string,
-    coins: number,
-  ): Promise<void> {
-    const userWallet = await this.walletRepository.findOne({
-      where: { ownerId: userId },
-    });
+		if (newPixoldBalance < 10000) {
+			sendNotification(
+				`@mbuslenko, @myroslavvv, There are less than 10,000 PXL left on Pixold`,
+			);
+		} else if (newPixoldBalance < 1000) {
+			sendNotification(
+				`@mbuslenko, @myroslavvv, There are less than 1,000 PXL left on Pixold`,
+			);
+		} else if (newPixoldBalance < 0) {
+			throw new InternalServerErrorException({
+				message: `Not enough coins in Pixold`,
+			});
+		}
 
-    const newUserBalance = userWallet.balanceInPXL - coins;
+		await this.walletRepository.update(
+			{ id: pixoldWallet.id },
+			{ balanceInPXL: newPixoldBalance },
+		);
+	}
 
-    if (newUserBalance < 0) {
-      throw new HttpException(
-        { message: `Not enough coins in your wallet` },
-        400,
-      );
-    }
+	async substractCoinsFromUserBalance(
+		userId: string,
+		coins: number,
+	): Promise<void> {
+		const userWallet = await this.walletRepository.findOne({
+			where: { ownerId: userId },
+		});
 
-    await this.walletRepository.update(
-      { id: userWallet.id },
-      { balanceInPXL: newUserBalance },
-    );
-  }
+		const newUserBalance = userWallet.balanceInPXL - coins;
 
-  async sendCoinsToPixold(userId: string, coins: number): Promise<void> {
-    const userWallet = await this.walletRepository.findOne({
-      where: { ownerId: userId },
-    });
-    const pixoldWallet = await this.walletRepository.findOne({
-      where: { ownerId: '6370046c-ac93-4d19-a55f-c59c5d5211b3' },
-    });
+		if (newUserBalance < 0) {
+			throw new HttpException(
+				{ message: `Not enough coins in your wallet` },
+				400,
+			);
+		}
 
-    if (!userWallet) {
-      throw new HttpException(
-        { message: `You don't have wallet to send coins` },
-        400,
-      );
-    }
+		await this.walletRepository.update(
+			{ id: userWallet.id },
+			{ balanceInPXL: newUserBalance },
+		);
+	}
 
-    const newUserBalance = userWallet.balanceInPXL - coins;
-    const newPixoldBalance = pixoldWallet.balanceInPXL + coins;
+	async sendCoinsToPixold(userId: string, coins: number): Promise<void> {
+		const userWallet = await this.walletRepository.findOne({
+			where: { ownerId: userId },
+		});
+		const pixoldWallet = await this.walletRepository.findOne({
+			where: { ownerId: '6370046c-ac93-4d19-a55f-c59c5d5211b3' },
+		});
 
-    if (newUserBalance < 0) {
-      throw new HttpException(
-        { message: `Not enough coins in your wallet` },
-        400,
-      );
-    }
+		if (!userWallet) {
+			throw new HttpException(
+				{ message: `You don't have wallet to send coins` },
+				400,
+			);
+		}
 
-    // ! TODO: Uncomment
-    // sendTransactionToPixold(userWallet.secretKey, coins.toString());
+		const newUserBalance = userWallet.balanceInPXL - coins;
+		const newPixoldBalance = pixoldWallet.balanceInPXL + coins;
 
-    await this.walletRepository.update(
-      { id: userWallet.id },
-      { balanceInPXL: newUserBalance },
-    );
-    await this.walletRepository.update(
-      { ownerId: '6370046c-ac93-4d19-a55f-c59c5d5211b3' },
-      { balanceInPXL: newPixoldBalance },
-    );
-  }
+		if (newUserBalance < 0) {
+			throw new HttpException(
+				{ message: `Not enough coins in your wallet` },
+				400,
+			);
+		}
+
+		// ! TODO: Uncomment
+		// sendTransactionToPixold(userWallet.secretKey, coins.toString());
+
+		await this.walletRepository.update(
+			{ id: userWallet.id },
+			{ balanceInPXL: newUserBalance },
+		);
+		await this.walletRepository.update(
+			{ ownerId: '6370046c-ac93-4d19-a55f-c59c5d5211b3' },
+			{ balanceInPXL: newPixoldBalance },
+		);
+	}
 }
